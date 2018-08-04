fcb86ebaffbfc5ada013efb23b7f39e2a73d575b
Frank Wagner <f.wagner@wagnersolutions.de>
2018-08-04 23:28:32 +0200

    implemented command options

--- src/NewCloudInitConfigDrive/Program.cs
+++ src/NewCloudInitConfigDrive/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using CommandLine;
 using Contiva.CloudInit.ConfigDrive;
 using Contiva.CloudInit.ConfigDrive.Generator;
 using Contiva.CloudInit.ConfigDrive.NoCloud;
@@ -11,46 +12,48 @@ namespace NewCloudInitConfigDrive
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            return Parser.Default.ParseArguments<NoCloudOptions>(args)
+                .MapResult(
+                    RunNoCloudAndReturnExitCode,
+                    errs => 1);
+        }
 
-            GeneratorBuilder.Init()
-                .NoCloud(new NoCloudConfigDriveMetaData("cool2"))
-                    .Content("T:\\openstack\\osbootstrap-init.zip")
-//                    .NetworkData(JObject.Parse(@"{
-//    'version' : '1',
-//    'config' :[ { 
-//        'type': 'physical',
-//        'name' : 'eth0',
-//        'mac_address' : '00:15:5d:ac:ac:9d',
-//        'subnets' : [ {
-//		    'type' : 'static',
-//		    'address' : '192.168.3.140/24',
-//		    'gateway' : '192.168.3.1',
-//		    'dns_nameservers':  [ '192.168.2.6', '192.168.2.9' ]
-//			}
-//            ]
-//        }
-//]
-//} 
-    
-//"))
-                    .UserData(JObject.Parse(
-                        @"{
-'password': 'password1', 
-'chpasswd': { 
-    'expire': false }, 
-'system_info' : {
-  'default_user': {
-    	'name': 'world'
-  } }
-        }"))
-                    .SwapFile()
-                    .ProxySettings(new ConfigDriveProxySettings())
-                .Processing()
-                    .Callback(r =>
+        private static int RunNoCloudAndReturnExitCode(NoCloudOptions opts)
+        {
+            try
+            {
+                var noCloudBuilder = GeneratorBuilder.Init()
+                    .NoCloud(new NoCloudConfigDriveMetaData(opts.Hostname));
+
+                if (!opts.NoSwapFile)
+                    noCloudBuilder.SwapFile();
+
+                if (!string.IsNullOrWhiteSpace(opts.Proxy))
+                    noCloudBuilder.ProxySettings(new ConfigDriveProxySettings(opts.Proxy) {NoProxy = opts.NoProxy});
+
+                if (!string.IsNullOrWhiteSpace(opts.UserData))
+                    noCloudBuilder.UserData(ReadJsonFile(opts.UserData));
+
+                if (!string.IsNullOrWhiteSpace(opts.NetworkData))
+                    noCloudBuilder.NetworkData(ReadJsonFile(opts.NetworkData));
+
+
+                if (!string.IsNullOrWhiteSpace(opts.Content))
+                    noCloudBuilder.Content(opts.Content);
+
+                var processingBuilder = noCloudBuilder.Processing();
+
+
+                if (!string.IsNullOrWhiteSpace(opts.ImagePath))
+                    processingBuilder.ImageFile(opts.ImagePath);
+                else
+                {
+                    processingBuilder.Callback(result =>
                     {
-                        foreach (var resultFile in r.Files)
+                        Console.WriteLine(result.MediaName);
+                        foreach (var resultFile in result.Files)
                         {
                             using (var reader = new StreamReader(resultFile.Content, Encoding.UTF8))
                             {
@@ -58,13 +61,58 @@ namespace NewCloudInitConfigDrive
                                 Console.WriteLine(reader.ReadToEnd());
                             }
                         }
-                        Console.WriteLine(r.MediaName);
-                    })
-                    .ImageFile("test.iso")
-                .Generate();
-
-
+                    });
+                }
+                
+                processingBuilder.Generate();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return -1;
+            }
         }
 
+        private static JObject ReadJsonFile(string filename)
+        {
+            return JObject.Parse(File.ReadAllText(filename));
+        }
     }
+
+    [Verb("NoCloud", HelpText = "Generate a NoCloud cloud-init disk")]
+    class NoCloudOptions
+    {
+        [Option(HelpText = "Path to cloud-init user data")]
+        public string UserData { get; set; }
+
+        [Option(HelpText = "Path to cloud-init network data")]
+        public string NetworkData { get; set; }
+
+        [Option(HelpText = "Path to content file")]
+        public string Content { get; set; }
+
+
+        [Option(Required = true)]
+        public string Hostname { get; set; }
+
+        [Option]
+        public string InstanceId { get; set; }
+
+
+        [Option("no-swapfile")]
+        public bool NoSwapFile { get; set; }
+
+        [Option("proxy")]
+        public string Proxy { get; set; }
+
+        [Option("no-proxy")]
+        public string NoProxy { get; set; }
+
+
+        [Option]
+        public string ImagePath { get; set; }
+
+    }
+
 }
