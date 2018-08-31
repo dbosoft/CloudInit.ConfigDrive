dd58351a5955b785dc8c3e94a86716d88f59e841
Frank Wagner <f.wagner@wagnersolutions.de>
2018-08-31 12:54:55 +0200

    moved ConfigException

---
+++ src/CloudInit.ConfigDrive.Core/CloudInitConfigurationException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Contiva.CloudInit.ConfigDrive
+{
+    [Serializable]
+    public class CloudInitConfigurationException : Exception
+    {
+        public CloudInitConfigurationException()
+        {
+        }
+
+        public CloudInitConfigurationException(string message) : base(message)
+        {
+        }
+
+        public CloudInitConfigurationException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected CloudInitConfigurationException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
